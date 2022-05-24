b6b9c01dec2f9d56478d50452c58ea5677def726
Milos Mladenovic <milos.m.mladenovic@htecgroup.com>
2022-05-24 12:20:53 +0200

    E2E tests week10

---
+++ e2e/src/test/java/com/projectname/e2e/tests/data/enums/Country.java
@@ -0,0 +1,41 @@
+package com.projectname.e2e.tests.data.enums;
+
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public enum Country {
+    SERBIA("Serbia", "RS", "Serbia (Србија)"),
+    SPAIN("Spain", "ES", "Spain (España)");
+
+    private final String countryName;
+    private final String twoLetterCountry;
+    private final String countryNumberSelect;
+
+    private Country(String countryName, String twoLetterCountry, String countryNumberSelect) {
+        this.countryName = countryName;
+        this.twoLetterCountry = twoLetterCountry;
+        this.countryNumberSelect = countryNumberSelect;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getTwoLetterCountry() {
+        return twoLetterCountry;
+    }
+
+    public String getCountryNumberSelect() {
+        return countryNumberSelect;
+    }
+
+    public static Country getRandomCountry() {
+        List<Country> countries = new ArrayList<>();
+        countries.add(Country.SERBIA);
+        countries.add(Country.SPAIN);
+        return countries.get(RandomUtils.nextInt(0, countries.size() - 1));
+    }
+}
---
+++ e2e/src/test/java/com/projectname/e2e/tests/suites/UpdateTests.java
@@ -0,0 +1,46 @@
+package com.projectname.e2e.tests.suites;
+
+import com.projectname.e2e.tests.asserts.PhpTravelAssert;
+import com.projectname.e2e.tests.data.model.UpdateUserRequest;
+import com.projectname.e2e.tests.data.model.UserPageDetails;
+import com.projectname.e2e.tests.data.provider.PhpTravels.PhpTravelsProvider;
+import com.projectname.e2e.tests.pages.NavigationBarPage;
+import com.projectname.e2e.tests.pages.UpdatePage;
+import com.projectname.e2e.tests.pages.UserPage;
+import com.projectname.e2e.tests.selectors.CustomBy;
+import com.projectname.e2e.tests.suites.common.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class UpdateTests extends TestBase {
+
+    @Test
+    public void verifyUserIsUpdated() {
+        NavigationBarPage navigationBarPage = new NavigationBarPage(getDriver(), "", "", "");
+        navigationBarPage.show();
+
+        UpdatePage updatePage = new UpdatePage(getDriver(), "", "", "");
+        updatePage.show();
+
+        updatePage.loginWithExistingUser();
+
+        UserPage userpage = new UserPage(getDriver(), "", "", "");
+        userpage.show();
+
+        UserPageDetails firstAndLastNameBeforeUpdate = userpage.getFirstAndLastNameOfUser();
+        System.out.println(firstAndLastNameBeforeUpdate);
+
+        WebElement updateButton = getDriver().findElement(CustomBy.xpath("/html/body/section/div/div[1]/div[1]/div[1]/div/div[3]/a"));
+        updateButton.click();
+
+        UpdateUserRequest userRequest = PhpTravelsProvider.prepareUpdateUserRequest();
+        updatePage.updateUserDetails(userRequest);
+
+        PhpTravelAssert phpTravelAssert = new PhpTravelAssert();
+        phpTravelAssert.assertUserDetailsAreUpdated(firstAndLastNameBeforeUpdate, userRequest);
+    }
+}
